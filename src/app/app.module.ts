33caf02b3af2022382ca07267a79f0561d06803c
scan.pratik@gmai.com <scan.pratik@gmai.com>
2020-10-08 00:21:12 +0530

    Hybrid Tab menu app with native camera

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy} from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import {StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { IonicStorageModule } from '@ionic/storage';
+import { HttpClientModule } from '@angular/common/http';
+import { Network } from '@ionic-native/network/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    IonicStorageModule.forRoot(),
+    HttpClientModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Network
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,103 @@
+import {Plugins, CameraResultType, CameraSource, CameraPhoto, Filesystem, FilesystemDirectory} from '@capacitor/core';
+import {Injectable} from '@angular/core';
+
+const {Camera} = Plugins;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PhotoService {
+
+    public photos: Photo[] = [];
+    private PHOTO_STORAGE = 'photos';
+
+    constructor() {
+    }
+
+
+
+    public async addNewToGallery() {
+        // Take a photo
+        const capturedPhoto = await Camera.getPhoto({
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            quality: 100
+        });
+
+        this.photos.unshift({
+            filepath: 'soon...',
+            webviewPath: capturedPhoto.webPath
+        });
+
+        // Save the picture and add it to photo collection
+        const savedImageFile = await this.savePicture(capturedPhoto);
+        this.photos.unshift(savedImageFile);
+
+        Storage.call(new Storage().setItem(this.PHOTO_STORAGE,
+            JSON.stringify(this.photos)));
+    }
+
+    private async savePicture(cameraPhoto: CameraPhoto) {
+
+        // Convert photo to base64 format, required by Filesystem API to save
+        const base64Data = await this.readAsBase64(cameraPhoto);
+
+        // Write the file to the data directory
+        const fileName = new Date().getTime() + '.jpeg';
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: FilesystemDirectory.Data
+        });
+
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+            filepath: fileName,
+            webviewPath: cameraPhoto.webPath
+        };
+    }
+
+    private async readAsBase64(cameraPhoto: CameraPhoto) {
+        // Fetch the photo, read as a blob, then convert to base64 format
+        // tslint:disable-next-line:no-non-null-assertion
+        const response = await fetch(cameraPhoto.webPath!);
+        const blob = await response.blob();
+
+        return await this.convertBlobToBase64(blob) as string;
+    }
+
+    convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+        // tslint:disable-next-line:new-parens
+        const reader = new FileReader;
+        reader.onerror = reject;
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+    })
+
+    public async loadSaved() {
+        // Retrieve cached photo array data
+        // const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+        // this.photos = JSON.parse(photoList.value) || [];
+
+        // Display the photo by reading into base64 format
+        for (const photo of this.photos) {
+            // Read each saved photo's data from the Filesystem
+            const readFile = await Filesystem.readFile({
+                path: photo.filepath,
+                directory: FilesystemDirectory.Data
+            });
+
+            // Web platform only: Load the photo as base64 data
+            photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+        }
+    }
+
+}
+
+export interface Photo {
+    filepath: string;
+    webviewPath: string;
+}
